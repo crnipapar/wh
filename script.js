30f8ff109a88de0395c8c1ffb48f2928c2874895
ivanmiku <ivan.mikulicic505@gmail.com>
2023-04-30 11:38:12 +0200

    connected parts (add or edit) - refreshes every 15 seconds - hotkey N added for add new item - default current date when adding new item

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 //ZA NOVU STAVKU DATUM DEFAULT VALUE = DANASNJI DAN
-//let currentDate = (document.getElementById("currentDate").valueAsDate =
-// new Date());
 
 const ip = "http://127.0.0.1:3000/";
 
@@ -105,7 +103,7 @@ const populateItems = async (data) => {
           onclick="addOrEditItem(${e.id})">Edit</button>\
           <button 
                 type="button"
-                class="btn btn-secondary"
+                class="btn btn-outline-dark"
                 data-bs-toggle="modal"
                 data-bs-target="#deleteItem"
                 style="font-size: 14px"
@@ -116,7 +114,7 @@ const populateItems = async (data) => {
             
               <button\
                 type="button" \
-                class="btn btn-secondary" \
+                class="btn btn-outline-dark" \
                 data-bs-toggle="modal" \
                 data-bs-target="#addPart"
                 style="font-size: 14px" \
@@ -163,10 +161,13 @@ const getItemsByOrderStatus = async (isOrderDone) => {
   let url = ip + "items/";
   if (isOrderDone === true) {
     url += "?orderDone=notnull";
+    toggleHeading(": riješene");
   } else if (isOrderDone === false) {
     url += "?orderDone=null";
+    toggleHeading(": aktivne");
   } else if (isOrderDone === "currentMonth") {
     url = `${ip}items/month/`;
+    toggleHeading(": tekući mjesec");
   }
 
   const data = await sendRequest(url, "GET");
@@ -200,9 +201,25 @@ const deleteConfirmed = async (id) => {
   const payload = new FormData();
   payload.append("id", id);
   await sendRequest(ip + "items/delete/", "DELETE", payload);
-  getItemsByOrderStatus();
   return;
 };
+
+// FUNCTION FOR REFRESHING THE LAST VIEWED PAGES by criteria: last button pressed = last triggered function is getting refreshed
+let lastCalledFunction = getItemsByOrderStatus; // initially set to default function
+
+// function to be called on button click
+const refreshOtherFunction = (func) => {
+  lastCalledFunction = func;
+  localStorage.setItem("lastCalledFunction", func.toString()); // store the function as a string
+  func();
+};
+
+// function to refresh the last called function
+const refreshLastCalledFunction = () => {
+  lastCalledFunction();
+};
+
+setInterval(refreshLastCalledFunction, 15000);
 
 const saveItem = async (id = "") => {
   let form = document.getElementById("ItemForm");
@@ -210,14 +227,230 @@ const saveItem = async (id = "") => {
   if (id !== "") {
     payload.append("id", id);
     const response = await sendRequest(ip + "items/edit/", "PUT", payload);
-    console.log(response);
-    getItemsByOrderStatus();
     return;
   }
   const response = await sendRequest(ip + "items/add/", "POST", payload);
+  return false;
+};
+
+// Function for heading text
+function toggleHeading(newText) {
+  var dynamicHeadingSpan = document.getElementById("heading");
+  dynamicHeadingSpan.textContent = " ";
+  dynamicHeadingSpan.textContent = newText;
+}
+
+// Funkcija za populate print
+const itemsPrint = async (id) => {
+  const response = await sendRequest(ip + `items/${id}`);
+  const e = response.data;
+
+  // Create new popup window for printing
+  const printWindow = window.open("");
+
+  // Populate the popup window with content
+  printWindow.document.write(`
+    <div>
+    <h2 style="padding-bottom: 25px; border-bottom: 2px solid black;  ;">Narudžba br. ${e.id}</h2>
+  </div>
+  
+  <div style="margin-top: 15px;">
+    <p style="font-size: 20px;">Klijent: <strong> ${e.name}</strong></p> 
+    <p>Datum kreiranja narudžbe: ${e.date}</p> \
+  
+  </div>
+  
+    <div style="margin-top: 30px">
+    <p>Proizvođač: <strong>${e.manufacturer}</strong></p>  
+    <p>Vrsta aparata i model: <strong>${e.type}</strong></p> 
+  </div>
+  
+  
+  <div style="margin-top: 30px">
+  <p>Narudžba gotova dana: <strong> ${e.orderDoneAt}</strong></p>   
+  <p>Klijent obaviješten dana: <strong>${e.customerNotifiedAt}</strong></p> 
+  
+  </div>
+  
+  <div style="margin-top: 50px">
+    <p>Polog EUR: <strong>${e.downpayment}</strong></p>
+    <p>Ukupno EUR: <strong> ${e.totalAmount} </strong></p>
+    
+  </div>
+  
+  
+  <div style="margin-top: 70px; border-top: 1px solid black; font-size: 12px;">
+    <p>Napomena</p>
+    <p>
+      ${e.note}
+    </p>
+  </div>
+  </div>`);
+
+  // Focus on the popup window and initiate print
+  printWindow.focus();
+  printWindow.print();
+  printWindow.close();
+};
+
+const getPartsById = async (id) => {
+  let url = ip + `items/parts/${id}`;
+  const payload = new FormData();
+  payload.append("itemID", id);
+  const data = await sendRequest(url, "GET");
+  populateParts(data.data);
+};
+
+// const savePart = async (id = "") => {
+
+const savePart = async (id = "") => {
+  let form = document.getElementById("PartForm");
+  let payload = new FormData(form);
+
+  if (id !== "") {
+    payload.append("id", id);
+    console.log("ID if the id is not null: " + id);
+    console.log(payload.description);
+    const response = await sendRequest(ip + `parts/edit/${id}`, "PUT", payload);
+    console.log(response);
+    return;
+  }
+  const response = await sendRequest(ip + "parts/add/", "POST", payload);
   console.log(response);
-  getItemsByOrderStatus();
   return;
+};
+
+const populateParts = async (data) => {
+  const partsContainer = document.getElementById("partsContainer");
+  partsContainer.innerHTML = "";
+
+  data.forEach((e) => {
+    let part = document.createElement("div");
+
+    part.innerHTML = `
+    <div class="part-container3">
+              <div class="parts-info">
+                <h2>Opis <strong> ${e.description}</strong></h2>
+              </div>
+              <div class="parts-date col">
+                <div>
+                  <div>
+                    <h2>Došao: <strong> ${e.arrivedAt}</strong></h2>
+                  </div>
+                  <div>
+                    <h2>Isporučen: <strong>${e.sentAt}</strong></h2>
+                  </div>
+                  <button
+                  onclick="addOrEditPart('${e.itemFK}', '${e.id}')"
+                    type="button"
+                    class="btn btn-outline-dark"
+                    data-bs-toggle="modal"
+                  >
+                    Uredi dio
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-outline-dark"
+                    data-bs-toggle="modal"
+                    data-bs-target="#deletePart"
+                  >
+                    Obriši dio
+                  </button>
+                </div>
+              </div>
+            </div>
+    `;
+    partsContainer.append(part);
+  });
+};
+
+const addOrEditPart = async (itemFK, id = "") => {
+  let response;
+
+  if (id != "") {
+    response = await sendRequest(ip + `parts/${id}`);
+    console.log("Value of description field: " + response.data.description);
+  }
+
+  let modal = document.getElementById("modalPart");
+  modal.innerHTML = `
+    <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5 mx-auto" id="staticBackdropLabel">
+              ${id !== "" ? "Uredi dio" : "Dodaj dio"}
+            </h1>
+
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <form id="PartForm">
+
+          <div class="mb-3" style="margin: 20px">
+            <label for="formGroupExampleInput" class="form-label"
+              >Opis dijela</label
+            >
+            <textarea
+              name="description"
+              class="form-control2"
+              style="height: 150px"
+              value="${id !== "" ? response.data.description : ""}"
+            >${id !== "" ? response.data.description : ""}</textarea>
+          </div>
+          <div class="modal-body">
+              <div style="display: grid; grid-template-columns: 50% 50%">
+                <div class="box mb-3">
+                  <label for="formGroupExampleInput" class="form-label"
+                    >Dio: Došao dana
+                  </label>
+                  <input type="date" name="arrivedAt" class="form-control" value="${
+                    id !== "" ? response.data.arrivedAt : ""
+                  }" />
+              
+                </div>
+
+                <div class="box mb-3">
+                  <label for="formGroupExampleInput" class="form-label"
+                    >Dio: Isporučen dana</label
+                  >
+                  <input type="date" name="sentAt" class="form-control" value="${
+                    id !== "" ? response.data.sentAt : ""
+                  }" />
+                  
+                </div>
+
+                <input type="hidden" name="itemFK" value="${itemFK}" />
+              </div>
+            </form>
+          </div>
+
+          <div class="modal-footer">
+            <button
+              
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Zatvori
+            </button>
+            <button
+              type="button"
+              id="addPartButton"
+              class="btn btn-primary"
+              data-bs-dismiss="modal"
+              onclick="${id !== "" ? `savePart(${id})` : "savePart()"}"
+            >
+              Spremi
+            </button>
+          </div>
+        </div>
+      </div>
+  `;
+  new bootstrap.Modal(modal).show();
 };
 
 // function to open a modal to add or edit a new item
@@ -325,7 +558,11 @@ const addOrEditItem = async (id = "") => {
                   name="date"
                   class="form-control"
                   id="currentDate"
-                  value="${id !== "" ? response.data.date : ""}"
+                  value="${
+                    id !== ""
+                      ? response.data.date
+                      : new Date().toISOString().slice(0, 10)
+                  }"
                 />
               </div>
               </div>
@@ -375,23 +612,29 @@ const addOrEditItem = async (id = "") => {
 
               <div class="modal-footer">
                 <button
+                id="modalCloseButton"
                   type="button"
                   class="btn btn-secondary btn-lg"
                   data-bs-dismiss="modal"
                 >
                   Zatvori
                 </button>
-                <button
+                <div
                   id="addNewItemButton"
-                  type="button"
                   class="btn btn-primary btn-lg"
-                  data-bs-dismiss="modal"
-                  onclick="${id !== "" ? `saveItem(${id})` : "saveItem()"}"
+                  
+          
+                  onclick="${
+                    id !== ""
+                      ? `saveItem(${id}, event)`
+                      : "saveItem(undefined, event)"
+                  };"
                 >
                   Spremi
-                </button>
+                </div>
               </div>
-            </form>
+              </form>
+
           </div>
         </div>
       </div>
@@ -399,207 +642,48 @@ const addOrEditItem = async (id = "") => {
   new bootstrap.Modal(modal).show();
 };
 
-// Function for heading text
-function toggleHeading(newText) {
-  var dynamicHeadingSpan = document.getElementById("heading");
-  dynamicHeadingSpan.textContent = " ";
-  dynamicHeadingSpan.textContent = newText;
-}
+document.addEventListener("keydown", function (event) {
+  // Check if the other modal is open
+  const otherModalBackdrop = document
+    .getElementById("modalPart")
+    .querySelector(".modal-backdrop");
+  const otherModal = document
+    .getElementById("modalPart")
+    .querySelector(".modal.show");
 
-// Funkcija za populate print
-const itemsPrint = async (id) => {
-  const response = await sendRequest(ip + `items/${id}`);
-  const e = response.data;
-
-  // Create new popup window for printing
-  const printWindow = window.open("");
-
-  // Populate the popup window with content
-  printWindow.document.write(`
-    <div>
-    <h2 style="padding-bottom: 25px; border-bottom: 2px solid black;  ;">Narudžba br. ${e.id}</h2>
-  </div>
-  
-  <div style="margin-top: 15px;">
-    <p style="font-size: 20px;">Klijent: <strong> ${e.name}</strong></p> 
-    <p>Datum kreiranja narudžbe: ${e.date}</p> \
-  
-  </div>
-  
-    <div style="margin-top: 30px">
-    <p>Proizvođač: <strong>${e.manufacturer}</strong></p>  
-    <p>Vrsta aparata i model: <strong>${e.type}</strong></p> 
-  </div>
-  
-  
-  <div style="margin-top: 30px">
-  <p>Narudžba gotova dana: <strong> ${e.orderDoneAt}</strong></p>   
-  <p>Klijent obaviješten dana: <strong>${e.customerNotifiedAt}</strong></p> 
-  
-  </div>
-  
-  <div style="margin-top: 50px">
-    <p>Polog EUR: <strong>${e.downpayment}</strong></p>
-    <p>Ukupno EUR: <strong> ${e.totalAmount} </strong></p>
-    
-  </div>
-  
-  
-  <div style="margin-top: 70px; border-top: 1px solid black; font-size: 12px;">
-    <p>Napomena</p>
-    <p>
-      ${e.note}
-    </p>
-  </div>
-  </div>`);
-
-  // Focus on the popup window and initiate print
-  printWindow.focus();
-  printWindow.print();
-  printWindow.close();
-};
-
-const savePart = async (id = "") => {
-  let form = document.getElementById("PartForm");
-  let payload = new FormData(form);
-  if (id !== "") {
-    payload.append("id", id);
-    const response = await sendRequest(ip + "parts/edit/", "PUT", payload);
-    console.log(response);
+  if (
+    otherModalBackdrop ||
+    otherModal ||
+    document.getElementById("modalPart").classList.contains("show")
+  ) {
     return;
   }
-  const response = await sendRequest(ip + "parts/add/", "POST", payload);
-  console.log(response);
-  return;
-};
 
-const populateParts = async (data) => {
-  const partsContainer = document.getElementById("partsContainer");
-  partsContainer.innerHTML = "";
-
-  data.forEach((e) => {
-    let part = document.createElement("div");
-
-    part.innerHTML = `
-    <div class="part-container3">
-              <div class="parts-info">
-                <h2>Opis <strong> ${e.name}</strong></h2>
-              </div>
-              <div class="parts-date col">
-                <div>
-                  <div>
-                    <h2>Došao: <strong> ${e.arrivedAt}</strong></h2>
-                  </div>
-                  <div>
-                    <h2>Isporučen: <strong>${e.sentAt}</strong></h2>
-                  </div>
-                  <button
-                    type="button"
-                    class="btn btn-outline-dark"
-                    data-bs-toggle="modal"
-                    data-bs-target="#addPart"
-                  >
-                    Uredi dio
-                  </button>
-                  <button
-                    type="button"
-                    class="btn btn-outline-dark"
-                    data-bs-toggle="modal"
-                    data-bs-target="#deletePart"
-                  >
-                    Obriši dio
-                  </button>
-                </div>
-              </div>
-            </div>
-    `;
-    partsContainer.append(part);
-  });
-};
-
-const getPartsById = async (id) => {
-  let url = ip + `items/parts/${id}`;
-  const payload = new FormData();
-  payload.append("itemID", id);
-  const data = await sendRequest(url, "GET");
-  populateParts(data.data);
-};
-
-const addOrEditPart = async (itemId, partId = "") => {
-  let response;
-  if (partId != "") {
-    response = await sendRequest(ip + `parts/${partId}}`);
-    console.log(response.data);
+  // If the other modal is not open, proceed with opening the new modal
+  if (event.key === "n") {
+    event.preventDefault(); // prevent the default "Bookmark" action
+    var existingModalBackdrop = document.querySelector(".modal-backdrop");
+    var existingModal = document.querySelector(".modal.show");
+    if (existingModalBackdrop) {
+      existingModalBackdrop.remove(); // remove any existing modal backdrop
+    }
+    if (existingModal) {
+      existingModal.classList.remove("show"); // remove the "show" class from any existing modals
+    }
+    document.getElementById("newItemButton").click(); // open the new modal
   }
-  let modal = document.getElementById("modalPart");
-  modal.innerHTML = `
-    <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5 mx-auto" id="staticBackdropLabel">
-              ${id !== "" ? "Uredi dio" : "Dodaj dio"}
-            </h1>
+});
 
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-
-          <div class="mb-3" style="margin: 20px">
-            <label for="formGroupExampleInput" class="form-label"
-              >Opis dijela</label
-            >
-            <textarea
-              name="note"
-              class="form-control2"
-              style="height: 200px"
-            > ${id !== "" ? response.data.description : ""}</textarea>
-          </div>
-          <div class="modal-body">
-            <form id="PartForm">
-              <div style="display: grid; grid-template-columns: 50% 50%">
-                <div class="box mb-3">
-                  <label for="formGroupExampleInput" class="form-label"
-                    >Dio: Došao dana
-                  </label>
-                  <input type="date" name="" class="form-control" />
-                  ${id !== "" ? response.data.arrivedAt : ""}
-                </div>
-
-                <div class="box mb-3">
-                  <label for="formGroupExampleInput" class="form-label"
-                    >Dio: Isporučen dana</label
-                  >
-                  <input type="date" class="form-control" />
-                  ${id !== "" ? response.data.sentAt : ""}
-                </div>
-              </div>
-            </form>
-          </div>
-
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-bs-dismiss="modal"
-            >
-              Zatvori
-            </button>
-            <button
-              type="button"
-              id="addPartButton"
-              class="btn btn-primary"
-              data-bs-dismiss="modal"
-              onclick="${id !== "" ? `savePart(${id})` : "savePart()"}"
-            >
-              Spremi
-            </button>
-          </div>
-        </div>
-      </div>
-  `;
-  new bootstrap.Modal(modal).show();
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const dateInput = document.querySelector("#currentDate");
+  if (dateInput !== null && dateInput.value === "") {
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const day = today.getDate();
+    const year = today.getFullYear();
+    const formattedDate = `${year}-${month < 10 ? "0" + month : month}-${
+      day < 10 ? "0" + day : day
+    }`;
+    dateInput.value = formattedDate;
+  }
+});
